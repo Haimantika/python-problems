7ec780643df2c29123e6511e9f45cd86a71d6d1e
Tanmoy Bhowmick <33360400+chingri@users.noreply.github.com>
2019-09-05 17:25:01 +0530

    Create prime-numbers-upto-given-input.py

---
+++ prime-numbers-upto-given-input.py
@@ -0,0 +1,16 @@
+N = int(input("enter a number:  "))
+
+def isprime(t):
+
+    for i in range(2,t):
+        if t<=1:
+            False
+        if t%i == 0:
+            return False
+
+    return True
+
+
+for num in range(2,N+1):
+    if isprime(num):
+      print(num, end =' ')
