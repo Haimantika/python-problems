fb06ae8ad1d7d1e4f32f25c07e3d416ee3729888
Tanmoy Bhowmick <33360400+chingri@users.noreply.github.com>
2019-09-18 17:40:29 +0530

    Create sort-array.py

---
+++ sort-array.py
@@ -0,0 +1,8 @@
+def sort(nums):
+    for i in range(len(nums)-1,0,1):
+        for j in range(i):
+            if nums[j]>nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+#now create a array and put in place of nums                
